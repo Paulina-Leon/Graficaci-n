6c73147bb1eff08b89b03e6a7e119bd021900a59
Paulina-Leon <L24170598@culiacan.tecnm.mx>
2026-02-17 10:25:02 -0700

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(800, 500);
+  background(180);
+}
+
+function draw() {
+  /*line(0,250,800,250);
+  line(400,0,400,800);
+  line(800,500,0,0);
+  line(0,500,800,0);
+  fill(0,0,255)
+  noStroke();
+  circle(400,250, 100);
+  fill(0)*/
+/*izquierda  */
+  stroke(0);
+  line(30,20,185,20);
+  stroke(126);
+  line(185,20,185,175);
+  stroke(255);
+  line(185,175,30,175);
+  line(30,175,30,20);
+/*derecha*/
+  stroke(0);
+  line(615,20,770,20);
+  stroke(126);
+  line(770,20,770,175);
+  stroke(255);
+  line(770,20,770,175);
+  line(30,175,30,20);
+
+  stroke(0);
+  line(30,20,185,20);
+  stroke(126);
+  line(185,20,185,175);
+  stroke(255);
+  line(185,175,30,175);
+  line(30,175,30,20);
+
+  stroke(0);
+  line(30,20,185,20);
+  stroke(126);
+  line(185,20,185,175);
+  stroke(255);
+  line(185,175,30,175);
+  line(30,175,30,20);
+
+
+
+}
