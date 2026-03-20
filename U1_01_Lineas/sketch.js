1a324365db3e19c52b6f34e00628c33618bbbc88
Paulina-Leon <L24170598@culiacan.tecnm.mx>
2026-03-20 02:50:20 +0000

    h

--- U1_01_Lineas/sketch.js
+++ U1_01_Lineas/sketch.js
@@ -1,22 +1,7 @@
 function setup() {
   createCanvas(800, 500);
-  background(180);
-}
-
+  background(180);}
 function draw() {
-  line(0,250,800,250);
-  stroke("purple");
-  line(400,0,400,800);
-  stroke(255, 0, 0);
-  line(800,500,0,0);
-  line(0,500,800,0);
-  fill(0,0,255)
-  noStroke();
-  circle(400,250, 100);
-  fill(0)
-
-
- 
   stroke(0);
   line(30,20,185,20);
   stroke(126);
@@ -25,34 +10,10 @@ function draw() {
   line(185,175,30,175);
   line(30,175,30,20);
 
-
-
-
   stroke(0);
   line(615,20,770,20);
   stroke(126);
   line(770,20,770,175);
   stroke(255);
   line(770,175,615,175);
-  line(615,175,615,20);
-
-
-  stroke(0);
-  line(615,250,770,250);
-  stroke(126);
-  line(770,250,770,405);
-  stroke(255);
-  line(770,405,615,405);
-  line(615,405,615,250); 
-
-
-
-  stroke(0);
-  line(30,250,185,250);
-  stroke(126);
-  line(185,250,185,405);
-  stroke(255);
-  line(185,405,30,405);
-  line(30,405,30,250);
-
-}
+  line(615,175,615,20);}
